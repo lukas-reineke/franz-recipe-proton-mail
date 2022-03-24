8985e6a257590889005c61c77299f4d182ef0ac6
Jake Cozart <jake@theflybook.com>
2022-03-24 11:34:46 -0700

    Fixed an issue with proton mail login and electrons screwup with atob and btoa

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+// Electron really screwed up here. atob and btoa are broken in recent versions, so override them.
+window.atob = data => Buffer.from(data, "base64").toString("latin1");
+window.btoa = data => Buffer.from(data, "latin1").toString("base64");
 module.exports = Franz => Franz;
